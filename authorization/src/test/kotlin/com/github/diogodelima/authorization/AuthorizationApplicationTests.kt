593f00b77204701ab2fd5f7259b889ff38ca29bd
Diogo Lima <diogocerqueiradeelima@gmail.com>
2024-11-25 17:25:18 +0000

    Initial Commit

---
+++ authorization/src/test/kotlin/com/github/diogodelima/authorization/AuthorizationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.diogodelima.authorization
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthorizationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
